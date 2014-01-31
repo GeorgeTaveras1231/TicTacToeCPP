edb93a772d94a9d1eb0caf2382bf01150fb5446b
George <george.taveras1231@gmail.com>
2014-01-31 12:56:35 -0500

    Update MainClass.cpp

--- MainClass.cpp
+++ MainClass.cpp
@@ -1,13 +1,4 @@
-//
-//  MainClass.cpp
-//  TicTacToe
-//
-//  Created by George Taveras on 10/31/13.
-//  Copyright (c) 2013 George Taveras. All rights reserved.
-//
-
 #include "MainClass.h"
-
 
 
 MainClass::MainClass()
