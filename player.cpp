72201242f1b30d1a92968a9f7bb05dff4ec7946d
George <george.taveras1231@gmail.com>
2014-01-31 13:00:28 -0500

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -1,10 +1,3 @@
-//
-//  player.cpp
-//  TicTacToe
-//
-//  Created by George Taveras on 10/25/13.
-//  Copyright (c) 2013 George Taveras. All rights reserved.
-//
 
 #include "player.h"
 #include "game_logic.h"
